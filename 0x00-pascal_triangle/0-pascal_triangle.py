8a30b1d6d302c0e89894466f8ec9f8669eb9241e
abenezert12 <abenezertekalign12@gmail.com>
2023-09-25 21:02:16 +0000

    TASKS

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,20 +2,23 @@
 """0x00-pascal_triangle"""
 
 def pascal_triangle(n):
-    """
-    Args:
-      n (int): The number of rows of the triangle
-    Returns:
-      List of lists of integers representing the Pascal's triangle
-    """
-    lists = []
-    if n == 0:
-        return lists
+    """Print the triangle"""
+    if n <= 0:
+        return []
+
+    pascal_triangle = [0] * n
+
     for i in range(n):
-        lists.append([])
-        lists[i].append(1)
-        if (i > 0):
-            for j in range(1, i):
-                lists[i].append(lists[i - 1][j - 1] + lists[i - 1][j])
-            lists[i].append(1)
-    return lists
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
+
+        for j in range(1, i):
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
+
+        pascal_triangle[i] = new_row
+
+    return pascal_triangle
