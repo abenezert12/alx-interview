e695bcc282c53699faa87b01caead064a3507eaf
Abenezer Tekalign <117775495+abenezert12@users.noreply.github.com>
2023-11-07 20:19:43 +0300

    TASKS

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,51 +1,17 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movieId = process.argv[2];
 
-function getMovieCharacters(movieId) {
-  const filmsUrl = 'https://swapi-api.alx-tools.com/api/films';
-
-  request.get(filmsUrl, (error, response, body) => {
-    if (response.statusCode === 200) {
-      const filmsData = JSON.parse(body);
-      const movieData = filmsData.results.find((film) => film.episode_id.toString() === movieId);
-
-      if (movieData) {
-        const charactersUrls = movieData.characters;
-        const characters = [];
-
-        for (const charUrl of charactersUrls) {
-          request.get(charUrl, (error, response, body) => {
-            if (response.statusCode === 200) {
-              const characterData = JSON.parse(body);
-              characters.push(characterData.name);
-              if (characters.length === charactersUrls.length) {
-                printCharacters(characters, movieData.title);
-              }
-            } else {
-              console.log(`Error: ${response.statusCode}`);
-            }
-          });
-        }
-      } else {
-        console.log(`Movie ID ${movieId} not found.`);
-      }
-    } else {
-      console.log(`Error: ${response.statusCode}`);
-    }
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
   });
-}
-
-function printCharacters(characters, movieTitle) {
-  console.log(`Characters in ${movieTitle}:`);
-  for (const character of characters) {
-    console.log(character);
-  }
-}
-
-if (movieId) {
-  getMovieCharacters(movieId);
-} else {
-  console.log('Usage: node 0-starwars_characters.js [Movie ID]');
-}
+};
